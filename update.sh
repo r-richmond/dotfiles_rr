eccf5de44a09491491d6e4877343ea17bce4a27b
r-richmond <rrichmond.gh@gmail.com>
2017-01-16 15:28:37 -0800

    correct configs for atom

--- update.sh
+++ update.sh
@@ -10,11 +10,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function doIt() {
   echo "updating atom config files"
-  rsync ~/.atom/config.cson "$DIR"/atom
-  rsync ~/.atom/init.coffee "$DIR"/atom
-  rsync ~/.atom/keymap.cson "$DIR"/atom
-  rsync ~/.atom/snippets.cson "$DIR"/atom
-  rsync ~/.atom/styles.less "$DIR"/atom
+  rsync ~/.atom/config.cson "$DIR"/.atom
+  rsync ~/.atom/init.coffee "$DIR"/.atom
+  rsync ~/.atom/keymap.cson "$DIR"/.atom
+  rsync ~/.atom/snippets.cson "$DIR"/.atom
+  rsync ~/.atom/styles.less "$DIR"/.atom
   echo  "updating atom package list"
   list_atom_packages > atom/.my_atom_packages
 }
