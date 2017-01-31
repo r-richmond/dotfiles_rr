713ab03d5661bf99d76db59cd43d533bf891a4c0
r-richmond <rrichmond.gh@gmail.com>
2017-01-31 11:08:07 -0800

    made apm silent, changed name of example.ini

--- crontab/scripts/nightly.sh
+++ crontab/scripts/nightly.sh
@@ -8,7 +8,7 @@ brew update;
 brew upgrade;
 brew cleanup;
 
-apm update;
+apm update --confirm false;
 
 #https://github.com/mathiasbynens/dotfiles/blob/master/.aliases alias lscleanup
 /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder;
