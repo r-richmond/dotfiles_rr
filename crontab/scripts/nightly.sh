cbb2b0ee7a79a7d4288e1a84891a5e25bb4106ec
r-richmond <rrichmond.gh@gmail.com>
2017-06-01 18:54:15 -0700

    dbeaver updates & cron change

--- crontab/scripts/nightly.sh
+++ crontab/scripts/nightly.sh
@@ -11,6 +11,6 @@ brew cleanup;
 apm update --confirm false;
 
 #https://github.com/mathiasbynens/dotfiles/blob/master/.aliases alias lscleanup
-/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder;
+#/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder;
 
 echo "`date` "$'\n'>> ~/Desktop/firedlog.md;
